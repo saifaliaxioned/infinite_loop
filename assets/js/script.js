91c96b33870338777d75c001a69870be02a029f2
saif <saifalis@axioned.com>
2022-10-14 16:50:47 +0530

    form validation function updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,22 @@
 // infinite loop functions
+var isValid = false;
 var navList = document.querySelectorAll('.nav-list');
 var slideBtn = document.querySelectorAll('.slide-btn');
 var hamburger = document.querySelector('.hamburger');
 var nextBtn = document.querySelector('.next-btn');
 var prevBtn = document.querySelector('.prev-btn');
+var staffCards = document.querySelector('.staff-cards');
+var galleryItems = document.querySelector('.gallery-items');
 var galleryList = document.querySelectorAll('.gallery-list img');
 var galleryModal = document.querySelector('.modalDiv');
-var modalImage =  document.querySelector('.modalDiv img')
+var modalImage = document.querySelector('.modalDiv img');
 var submitModal = document.querySelector('.submitModal');
 var inputName = document.querySelector('#name');
 var email = document.querySelector('#email');
 var textArea = document.querySelector('#textarea');
 var form = document.querySelector('#contactForm');
 var submitBtn = document.querySelector('.input-control input');
-var stringPattern = /^[a-zA-Z]+$/g;
-var messagePattern = /^[a-zA-Z\s]+$/g;
-var emailPattern = /^[A-Za-z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/;
+
 
 // nav function 
 navList.forEach(function (list) {
@@ -41,116 +42,92 @@ slideBtn.forEach(function (staff) {
     slider();
   });
 });
-
-
-var staffCards = document.querySelector('.staff-cards');
-var galleryItems = document.querySelector('.gallery-items');
-
-// function slider(num) {
-//   staffCards.scrollBy({
-//     left: num,
-//     behavior: 'smooth'
-//   });
-// }
-
 function slider(num) {
-  galleryItems.scrollBy({
+  staffCards.scrollBy({
     left: num,
     behavior: 'smooth'
   });
 }
-
 function cardSlider(size) {
-
   nextBtn.addEventListener('click', function () {
     slider(size);
   })
   prevBtn.addEventListener('click', function () {
     slider(-size);
   })
-
 }
-
-if (window.innerWidth > 769 && window.innerWidth <= 995) {
+if (window.innerWidth > 769 && window.innerWidth <= 1440) {
   cardSlider(821);
 } else if (window.innerWidth > 640 && window.innerWidth <= 768) {
   cardSlider(690);
 }
 
-
 // submit function
 form.addEventListener('submit', function (e) {
   e.preventDefault();
-  var activeErrors = document.querySelectorAll('.show-error');
-  if (inputName.value  && textArea.value && email.value  && (activeErrors.length == 0)) {
-    console.log('submitted');
+  if (isValid) {
     submitModal.classList.remove('hide-content');
     document.documentElement.classList.add('removeScroll');
-  } else {
-    console.log('fill required fields');
-    validateError(inputName, stringPattern, "*whitespace and numbers are not allowed", 3);
-    validateError(textArea, messagePattern, "*whitespace and numbers are not allowed", 10);
-    validateError(email, emailPattern, "*invalid email");
+    for (var i = 0; i < 3; i++) {
+      form.children[i].children[0].classList.remove('success');
+    }
+    form.reset();
+  }
+  else {
+    var str = 'field is required';
+    for (var i = 0; i < 3; i++) {
+      var formValue = form.children[i].children[0].value;
+      if (formValue == '') {
+        form.children[i].children[1].innerText = str;
+        form.children[i].children[1].classList.add('show-error');
+        form.children[i].children[0].classList.add('error');
+      }
+    }
   }
 });
 
-// onBlur input function
-inputName.addEventListener('blur', function () {
-  validateError(inputName, stringPattern, "*whitespace and numbers are not allowed", 3);
-});
+// error function
 
-textArea.addEventListener('blur', function () {
-  validateError(textArea, messagePattern, "*whitespace and numbers are not allowed", 10);
-});
+for (var i = 0; i < 3; i++) {
+  form.children[i].children[0].addEventListener('blur', validateError);
+}
 
-email.addEventListener('blur', function () {
-  validateError(email, emailPattern, "*invalid email");
-});
-
-// error function 
-function validateError(input, pattern, message, Length = '') {
-  var inputError = input.parentElement.children;
-  for (var i = 0; i < inputError.length; i++) {
-    if (inputError[i].classList.contains('err')) {
-      if (input.value) {
-        if (input.value.match(pattern)) {
-          if (inputError[0].classList.contains('textInput')) {
-            if (input.value.length > Length) {
-              input.classList.add('error');
-              inputError[i].classList.add('show-error');
-              inputError[i].innerText = '*only 10 characters are allowed';
-            } else if (input.value.length < 3) {
-              input.classList.add('error');
-              inputError[i].classList.add('show-error');
-              inputError[i].innerText = '*characters must be more than 3 characters';
-            }
-             else {
-              input.classList.remove('error');
-              inputError[i].classList.remove('show-error');
-              input.classList.add('success');
-            }
-          } else {
-            if (input.value.length < Length) {
-              input.classList.add('error');
-              inputError[i].classList.add('show-error');
-              inputError[i].innerText = '*characters must be more than 3 characters';
-            } else {
-              input.classList.remove('error');
-              inputError[i].classList.remove('show-error');
-              input.classList.add('success');
-            }
-          }
-        } else {
-          input.classList.add('error');
-          inputError[i].classList.add('show-error');
-          inputError[i].innerText = message;
-        }
-      } else {
-        input.classList.add('error');
-        inputError[i].classList.add('show-error');
-        inputError[i].innerText = '*field is required';
+function validateError() {
+  this.nextElementSibling.classList.remove('show-error');
+  this.classList.remove('error');
+  this.classList.add('success');
+  var value = this.value.trim();
+  var max = 20;
+  var min = 3;
+  var stringPattern = /^[a-zA-Z\s]+$/g;
+  var emailPattern = /^[A-Za-z0-9._]{3,}@[A-Za-z]{3,}[.]{1}[A-Za-z.]{2,6}$/;
+  isValid = true;
+  try {
+    if (this.className == 'textInput') {
+      max = 50;
+    }
+    if (value === '') {
+      isValid = false;
+      throw '*field is required';
+    } else if (this.classList.contains('email')) {
+      if (!emailPattern.test(value)) {
+        isValid = false;
+        throw '*invalid email';
+      }
+    } else {
+      if (value.length < min || value.length > max) {
+        isValid = false;
+        throw '*characters must be more than 3 characters and less than ' + max;
+      } else if (!stringPattern.test(this.value)) {
+        isValid = false;
+        throw '*numbers are not allowed';
       }
     }
+  } catch (e) {
+    this.nextElementSibling.innerText = e;
+    this.nextElementSibling.classList.add('show-error');
+    this.classList.remove('sucess');
+    this.classList.add('error');
   }
 };
 
@@ -163,30 +140,18 @@ galleryList.forEach(function (list) {
     document.documentElement.classList.add('removeScroll');
   });
 });
-
 galleryModal.addEventListener('click', function (e) {
-  if (e.target === galleryModal || e.target === galleryModal.children[0].children[0]) {
+  if (e.target === galleryModal || e.target === galleryModal.children[0] || e.target === galleryModal.children[0].children[0]) {
     galleryModal.classList.add('hide-content');
     document.documentElement.classList.remove('removeScroll');
   };
-});
-
+}); 
 submitModal.addEventListener('click', function (e) {
-  if (e.target === submitModal || e.target === submitModal.children[0].children[1]) {
+  if (e.target === submitModal || e.target === submitModal.children[0].children[1].children[0]) {
     submitModal.classList.add('hide-content');
     document.documentElement.classList.remove('removeScroll');
   };
 });
-
-
-
-
-
-
-
-
-
-
 
 
 
